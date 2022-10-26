0c215b3d01acf854f506fd529ec5d0aee12670c0
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-10-26 15:31:22 +0800

    Update baiduPan.js

--- script/baiduPan.js
+++ script/baiduPan.js
@@ -3,7 +3,7 @@
 百度网盘 解锁在线视频倍率/清晰度
 
 [Script]
-http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/fhqievg/rules/main/script/baiduPan.js
+http-response ^https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/fhqievg/rules/main/script/baiduPan.js
 
 [MITM]
 hostname = pan.baidu.com
