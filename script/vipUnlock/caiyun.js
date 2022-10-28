4ad870d9f6017466ca8ee8b3fef14dae70bcd890
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-10-28 16:08:38 +0800

    Update caiyun.js

--- script/vipUnlock/caiyun.js
+++ script/vipUnlock/caiyun.js
@@ -4,4 +4,4 @@ obj.result["svip_expired_at"]=4096483190
 obj.result["is_xy_vip"]=true
 obj.result["is_vip"]=true
 obj.result["vip_type"]="s"
-$done({body:JSON.stringify(obj)})
+$done({body:JSON.stringify(obj)});
