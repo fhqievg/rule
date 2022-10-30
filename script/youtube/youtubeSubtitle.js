cbd6c0c805291a8ef94a7b486634b8831f98d1a9
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-10-31 01:18:53 +0800

    Update youtubeSubtitle.js

--- script/youtube/youtubeSubtitle.js
+++ script/youtube/youtubeSubtitle.js
@@ -17,22 +17,29 @@ let options = {
 }
 
 $httpClient.get(options, function (error, response, data) {
-    if (setting.line == "sl") $done({ body: data })
+    if (setting.line == "sl") {
+        $done({ body: data })
+    }
     
-    let timeline = body.match(/<p t="\d+" d="\d+">/g)
-    if (url.match(/&kind=asr/)) {
-        body = body.replace(/<\/?s[^>]*>/g, "")
-        data = data.replace(/<\/?s[^>]*>/g, "")
-        timeline = body.match(/<p t="\d+" d="\d+"[^>]+>/g)
-    }
+    switch (setting.line) {
+        case "f":
+        case "s":
+            let timeline = body.match(/<p t="\d+" d="\d+">/g)
+            if (url.match(/&kind=asr/)) {
+                body = body.replace(/<\/?s[^>]*>/g, "")
+                data = data.replace(/<\/?s[^>]*>/g, "")
+                timeline = body.match(/<p t="\d+" d="\d+"[^>]+>/g)
+            }
 
-    for (var i in timeline) {
-        let patt = new RegExp(`${timeline[i]}([^<]+)<\\/p>`)
-        if (body.match(patt) && data.match(patt)) {
-            if (setting.line == "s") body = body.replace(patt, `${timeline[i]}$1\n${data.match(patt)[1]}</p>`)
-            if (setting.line == "f") body = body.replace(patt, `${timeline[i]}${data.match(patt)[1]}\n$1</p>`)
-        }
+            for (var i in timeline) {
+                let patt = new RegExp(`${timeline[i]}([^<]+)<\\/p>`)
+                if (body.match(patt) && data.match(patt)) {
+                    if (setting.line == "f") body = body.replace(patt, `${timeline[i]}${data.match(patt)[1]}\n$1</p>`)
+                    if (setting.line == "s") body = body.replace(patt, `${timeline[i]}$1\n${data.match(patt)[1]}</p>`)
+                }
+            }
+            $done({ body })
+        default:
+            $done({ body: data })
     }
-
-    $done({ body })
 })
