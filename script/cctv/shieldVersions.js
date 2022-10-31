2225844130461e4628d6aea8092d15f9bda58167
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-10-31 12:48:26 +0800

    Update shieldVersions.js

--- script/cctv/shieldVersions.js
+++ script/cctv/shieldVersions.js
@@ -1,8 +1,3 @@
 let obj = JSON.parse($response.body)
-obj.data["versionsUrl"] = ''
-obj.data["versionsinfo"] = ''
-obj.data["versionsNum"] = "7100"
-obj.data["versionsMin"] = "7.0.3"
-obj.data["versionsName"] = "7.1.0"
-obj.data["isForce"] = "1"
+obj.data["isForce"] = "3"
 $done({body: JSON.stringify(obj)});
