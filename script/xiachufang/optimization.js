3881a400e61816e48257438ae76b609a60f12a83
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-11-03 14:45:15 +0800

    Update optimization.js

--- script/xiachufang/optimization.js
+++ script/xiachufang/optimization.js
@@ -1,6 +1,7 @@
 let obj = JSON.parse($response.body)
 let url = $request.url
 
+//个人中心
 if (url.match(/account/)) {
    if (url.match(/api/)) {
        //obj.account.is_admin = true
@@ -20,4 +21,16 @@ if (url.match(/get_profile/)) {
   obj.prime_portal = []
 }
 
-$done({body:JSON.stringify(obj)});
+//分类
+if (url.match(/api/) && url.match(/ranking_list/)) {
+   let caregory = []
+   for (var i in obj.cells) {
+      let filed = obj.cells[i].GetHomePageVerticalRankingListSectionRespCell_cell_OneOfCase
+      if (filed != "side_slip_activity_cell" && filed != "dish_square_cell") {
+          caregory.push(obj.cells[i])
+      }
+   }
+   obj.cells = caregory
+}
+
+$done({ body:JSON.stringify(obj) });
