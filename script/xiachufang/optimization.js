61d438294ac2ab7a9912c05cd054ae2b9e21edde
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-11-09 16:00:04 +0800

    Update optimization.js

--- script/xiachufang/optimization.js
+++ script/xiachufang/optimization.js
@@ -2,27 +2,27 @@ let obj = JSON.parse($response.body)
 let url = $request.url
 
 //个人中心
-if (url.match(/account/)) {
-   if (url.match(/api/)) {
+if (url.includes("account")) {
+   if (url.includes("api")) {
        //obj.account.is_admin = true
        obj.account.user.ip_location = ""
        obj.prime_portal = []
    }
    
-   if (url.match(/www/)) {
+   if (url.includes("www")) {
        //obj.content.is_admin = true
        obj.content.ip_location = ""
        obj.content.prime_portal = []
    }
 }
 
-if (url.match(/get_profile/)) {
+if (url.includes("get_profile")) {
   obj.user.ip_location = ""
   obj.prime_portal = []
 }
 
 //分类
-if (url.match(/api/) && url.match(/ranking_list/)) {
+if (url.includes("api") && url.includes("ranking_list")) {
    let caregory = []
    for (var i in obj.cells) {
       let filed = obj.cells[i].GetHomePageVerticalRankingListSectionRespCell_cell_OneOfCase
@@ -34,7 +34,7 @@ if (url.match(/api/) && url.match(/ranking_list/)) {
 }
 
 //推荐数据流
-if (url.match(/paged_waterfall_recommendations/)) {
+if (url.includes("paged_waterfall_recommendations")) {
    let data = []
    for (var i in obj.multiplex_cells) {
        let label = obj.multiplex_cells[i].recommendation_cell.label
@@ -46,13 +46,13 @@ if (url.match(/paged_waterfall_recommendations/)) {
    obj.multiplex_cells = data
 }
 
-if (url.match(/init_page/)) {
+if (url.includes("init_page")) {
    if (typeof obj.content.chustory_tab.banners != 'undefined' && obj.content.chustory_tab.banners.length > 0) {
        let bannersObj = obj.content.chustory_tab.banners
        let bannersData = []
        for (var i in bannersObj) {
            let slot_name = bannersObj[i].object.slot_name
-           if (!slot_name.match(/_ad/)) {
+           if (!slot_name.includes("_ad")) {
                bannersData.push(bannersObj[i])
            }
        }
