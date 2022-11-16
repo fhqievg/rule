a36b9b90468630e979cd7385f4e9a7eabe40f6a7
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-11-16 08:38:39 +0800

    更新 removeGzhArticleAd.js

--- script/wx/removeGzhArticleAd.js
+++ script/wx/removeGzhArticleAd.js
@@ -1,13 +1,3 @@
-/*
-************************
-微信 去除公众号文章底部广告
-[Script]
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/fhqievg/rules/main/script/wx.js
-[MITM]
-hostname = mp.weixin.qq.com
-*************************
-*/
-
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
