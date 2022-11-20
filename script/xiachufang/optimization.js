0c711e824ab31612ce45cf6f67ed17bdadcdc6ec
fhqievg <30724814+fhqievg@users.noreply.github.com>
2022-11-20 08:57:18 +0800

    更新 optimization.js

--- script/xiachufang/optimization.js
+++ script/xiachufang/optimization.js
@@ -60,4 +60,10 @@ if (url.includes("init_page")) {
    }
 }
 
+//详情
+if (url.includes("detail_")) {
+  if (obj.recipe.overall_rating != 'undefined')
+   obj.recipe.overall_rating.dishes = []
+}
+
 $done({ body:JSON.stringify(obj) });
