4d8130ec0880fc2b9114a9686682289e4a3faf78
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-03-31 16:33:26 +0800

    Update 12306.js

--- script/12306.js
+++ script/12306.js
@@ -1,4 +1,20 @@
-console.log("top1");
+console.log("top3");
+let body = '{"code":"00","message":"无广告返回"}';
+if(!$request.body){
+    $done({ body });
+}
 let obj = JSON.parse($request.body);
-console.log("top2");
+if (typeof obj.placementNo != 'undefined') {
+    switch (obj.placementNo) {
+        case '0007':
+            body = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+            break;
+        case 'G0054':
+            body = '{"code":"00","materialsList":[]}';
+            break;
+        default:
+            break;
+    }
+}
+$done({ body });
 
