d82bc1e8cfd4ebf0acd10e412e854fee9521dee9
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-03-31 18:22:37 +0800

    Update 12306.js

--- script/12306.js
+++ script/12306.js
@@ -1,22 +1,12 @@
-console.log("top10");
+let obj = JSON.parse($response.body);
 let body = '{"code":"00","message":"无广告返回"}';
-console.log($request.url);
-console.log($request);
-console.log("top6");
-if(!$request.body){
-    $done({ body });
-}
-let obj = JSON.parse($request.body);
-if (typeof obj.placementNo != 'undefined') {
-    switch (obj.placementNo) {
-        case '0007':
-            body = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-            break;
-        case 'G0054':
-            body = '{"code":"00","materialsList":[]}';
-            break;
-        default:
-            break;
+if (typeof obj.advertParam != 'undefined' && typeof obj.advertParam.showSkipBtn != 'undefined') {
+    if (obj.advertParam.showSkipBtn == 1) {
+        //0007
+        body = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+    } else {
+        //G0054
+        body = '{"code":"00","materialsList":[]}';
     }
 }
 $done({ body });
