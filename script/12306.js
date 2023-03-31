139f52193fe7fa6256588ebed70c59bf8157677a
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-03-31 16:20:05 +0800

    Update 12306.js

--- script/12306.js
+++ script/12306.js
@@ -1,19 +1,4 @@
+console.log("top1");
 let obj = JSON.parse($request.body);
-let data = '{"code":"00","message":"无广告返回"}';
-let logMsg = $request.url + "\r\n\r\n\r\n" + $request.body;
-$notification.post("第一步", "接口参数", logMsg);
-if (typeof obj.placementNo != 'undefined') {
-    $notification.post("第二步", "placementNo", obj.placementNo);
-    switch (obj.placementNo) {
-        case '0007':
-            data = '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-            break;
-        case 'G0054':
-            data = '{"code":"00","materialsList":[]}';
-            break;
-        default:
-            break;
-    }
-    $notification.post("第三步", "data", data);
-}
-$done({body: data});
+console.log("top2");
+
