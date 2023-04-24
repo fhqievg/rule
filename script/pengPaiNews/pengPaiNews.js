7aed2c03de6c3b6462efee6ec481aec94ca525f9
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-04-25 00:07:02 +0800

    Update pengPaiNews.js

--- script/pengPaiNews/pengPaiNews.js
+++ script/pengPaiNews/pengPaiNews.js
@@ -40,23 +40,24 @@ function dataHandle(contList) {
             continue;
         }
 
-        //轮播文章：103
+        //头部轮播文章：103
         switch (contList[i].cardMode) {
             case '106':
                 //中间模块
                 contList[i].childList = contList[i].childList.filter(
                     (item) =>
-                        //item.cardMode != '107' &&  //24h
-                        //item.cardMode != '108' &&  //早晚
-                        //item.cardMode != '109' &&  //消费
-                        //item.cardMode != '110' &&  //热评
+                        //item.cardMode != '107'  //24h
+                        //item.cardMode != '108'  //早晚
+                        //item.cardMode != '109'  //消费
+                        //item.cardMode != '110'  //热评
                         item.cardMode != '133'  //填字
                 );
                 handleResult.push(contList[i]);
                 break;
             default:
-                //过滤头部文章(102)、ad(5)、专题(117)、宣传栏(121)
-                if (contList[i].cardMode != '102' && contList[i].cardMode != '5' && contList[i].cardMode != '117' && contList[i].cardMode != '121') {
+                //过滤头部文章(102)、ad(5)、专题(117)、官方网站(121)
+                let filterArr = ['102','5','117','121'];
+                if(filterArr.indexOf(contList[i].cardMode) === -1){
                     handleResult.push(contList[i]);
                 }
                 break;
