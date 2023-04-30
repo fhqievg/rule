0b5a77fd22ecb5c07ca6df04d1ae098e61f09c67
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-04-30 14:19:11 +0800

    Create amdc.js

---
+++ script/amdc.js
@@ -0,0 +1,15 @@
+if (!$response.body) $done({});
+
+let ua = $request.headers["User-Agent"];
+if (ua.includes("AMap") || ua.includes("Cainiao")) {
+    try {
+        let obj = JSON.parse($response.body);
+        obj = {}
+        $done({ body: JSON.stringify(obj) });
+    } catch (err) {
+        let amdc = 'e30=';
+        $done({ body: amdc });
+    }
+}else{
+    $done({});
+}
