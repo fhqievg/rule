e30f1f134b169b5c12d43d5eb678562d6cbb6a27
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-05-04 18:08:15 +0800

    Create 555.js

---
+++ script/video/555.js
@@ -0,0 +1,29 @@
+let obj = JSON.parse($response.body)
+let url = $request.url
+
+//首页
+if (url.includes('movie/index_recommend')) {
+    if (typeof obj.data != 'undefined' && obj.data.length > 0) {
+        obj.data = obj.data.filter(
+            (item) =>
+                item.layout != 'advert_self'
+        );
+
+        for (let i in obj.data) {
+            switch (obj.data[i].layout) {
+                //轮播
+                case 'index_recommend_carousel':
+                    obj.data[i].list = obj.data[i].list.filter(
+                        (item) =>
+                            item.type === 1
+                    );
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
+$done({ body: JSON.stringify(obj) });
