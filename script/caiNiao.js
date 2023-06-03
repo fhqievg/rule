1506f2fc5287331466eb989147fe653898833215
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-06-03 19:31:51 +0800

    Update and rename caiNiaoGuoGuo.js to caiNiao.js

--- script/caiNiao.js
+++ script/caiNiao.js
@@ -2,9 +2,9 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("nbpresentation.homepage.merge.get.cn")) {
+if (url.includes("nbpresentation.homepage.merge.get")) {
+    // 反馈组件
     if (obj.data) {
-        // 移除 反馈组件
         const item = [
             "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@0",
             "mtop.cainiao.nbmensa.research.researchservice.acquire.cn@1",
@@ -17,7 +17,8 @@ if (url.includes("nbpresentation.homepage.merge.get.cn")) {
             }
         }
     }
-} else if (url.includes("nbpresentation.pickup.empty.page.get.cn")) {
+} else if (url.includes("nbpresentation.pickup.empty.page.get")) {
+    // 取件页面
     if (obj.data.result) {
         let ggContent = obj.data.result.content;
         if (ggContent.middle) {
@@ -31,25 +32,41 @@ if (url.includes("nbpresentation.homepage.merge.get.cn")) {
             );
         }
     }
-} else if (url.includes("nbpresentation.protocol.homepage.get.cn")) {
+} else if (url.includes("nbpresentation.protocol.homepage.get")) {
+    // 首页
     if (obj.data.result) {
         let res = obj.data.result;
         if (res.dataList) {
             res.dataList = res.dataList.filter((i) => {
-                // 顶部图标
-                if (i.type.includes("icons_scroll_unable")) {
+                if (i.type.includes("kingkong")) {
+                    if (i.bizData.items) {
+                        for (let ii of i.bizData.items) {
+                            ii.rightIcon = null;
+                            ii.bubbleText = null;
+                        }
+                        return true;
+                    }
+                } else if (i.type.includes("icons_scroll")) {
+                    // 顶部图标
                     if (i.bizData.items) {
                         const item = [
                             "bgxq", // 包裹星球
+                            "cncy", // 填字赚现金
                             "cngy", // 免费领水果
                             "cngreen", // 绿色家园
                             "gjjf", // 裹酱积分
+                            "jkymd", // 集卡赢免单
                             "ljjq", // 领寄件券
                             "ttlhb" // 天天领红包
                         ];
                         i.bizData.items = i.bizData.items.filter(
                             (ii) => !item.includes(ii.key)
                         );
+                        for (let ii of i.bizData.items) {
+                            ii.rightIcon = null;
+                            ii.bubbleText = null;
+                        }
+                        return true;
                     }
                 } else if (i.type.includes("big_banner_area")) {
                     // 新人福利
@@ -60,38 +77,48 @@ if (url.includes("nbpresentation.homepage.merge.get.cn")) {
                 } else {
                     return true;
                 }
-                res.dataList.forEach((i) => {
-                    i.bizData.items.forEach((ii) => {
-                        ii.rightIcon = null;
-                        ii.bubbleText = null;
-                    });
-                });
             });
         }
     }
-} else if (url.includes("guoguo.nbnetflow.ads.show.cn")) {
+} else if (url.includes("guoguo.nbnetflow.ads.show")) {
+    $notification.post("show", "", $response.body);
     // 我的页面
     if (obj.data.result) {
         obj.data.result = obj.data.result.filter(
             (i) =>
-                ![
-                    "29524", // 开屏广告
-                    "29766",
-                    "30656", // 30656-30659 休闲娱乐
-                    "30657",
-                    "30658",
-                    "30659",
-                    "31491",
-                    "31627",
-                    "31769", // 帮同学取
-                    "31788", // 签到领红包
-                    "32103", // 攻略
-                    "32926", // 出库码推广
-                    "33114",
-                    "33116",
-                    "33122"
-                ].includes(i?.id)
+                !(
+                    i?.materialContentMapper?.adItemDetail ||
+                    (i?.materialContentMapper?.bgImg &&
+                        i?.materialContentMapper?.advRecGmtModifiedTime) ||
+                    i?.materialContentMapper?.group_id?.includes("entertainment")
+                )
         );
+    }
+} else if (url.includes("guoguo.nbnetflow.ads.mshow")) {
+    // 首页
+    if (obj.data) {
+        const item = [
+            "10", // 物流详情页 底部横图
+            "498", // 物流详情页 左上角
+            "328", // 3位数为家乡版本
+            "366",
+            "369",
+            "615",
+            "616",
+            "727",
+            "793", // 支付宝 小程序 搜索框
+            "954", // 支付宝 小程序 置顶图标
+            "1308", // 支付宝 小程序 横图
+            "1316", // 头部 banner
+            "1332", // 我的页面 横图
+            "1340", // 查快递 小妙招
+            "1391" // 支付宝 小程序 寄包裹
+        ];
+        for (let i of item) {
+            if (obj.data?.[i]) {
+                delete obj.data[i];
+            }
+        }
     }
 }
 
