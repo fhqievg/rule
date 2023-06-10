54c65935f618a83d67ce0c71da8c2540fb52cb00
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-06-10 15:07:44 +0800

    更新 exchangeRate.js

--- script/crontab/exchangeRate.js
+++ script/crontab/exchangeRate.js
@@ -9,23 +9,24 @@ const ENABLE_RATES_SHOW = true;
 const TACTIC_HANDOFF = [100, 101, 102];
 const API_CONFIG = {
     "A": {
+        "isCode": true
         "tactic": 201,
         "information": [
-            {"apiInterface": "v4", "apiUrl": "https://api.exchangerate-api.com/v4/latest/CNY"},
-            {"apiInterface": "v6", "apiUrl": "https://open.er-api.com/v6/latest/CNY"},
+            {"apiInterface": "v4", "apiUrl": "aHR0cHM6Ly9hcGkuZXhjaGFuZ2VyYXRlLWFwaS5jb20vdjQvbGF0ZXN0L0NOWQ=="},
+            {"apiInterface": "v6", "apiUrl": "aHR0cHM6Ly9vcGVuLmVyLWFwaS5jb20vdjYvbGF0ZXN0L0NOWQ=="},
         ]
     },
     "B": {
+        "isCode": true
         "tactic": TACTIC_HANDOFF[0],
         "information": [
-            //&currencies=CNY
             {
                 "apiInterface": "vu",
-                "apiUrl": "http://api.currencylayer.com/live?access_key=5298ce0787f0c48e3810e8aa1cd78ed6&format=1"
+                "apiUrl": "aHR0cDovL2FwaS5jdXJyZW5jeWxheWVyLmNvbS9saXZlP2FjY2Vzc19rZXk9NTI5OGNlMDc4N2YwYzQ4ZTM4MTBlOGFhMWNkNzhlZDYmc291cmNlPVVTRCZmb3JtYXQ9MQ=="
             },
             {
                 "apiInterface": "vc",
-                "apiUrl": "http://api.currencylayer.com/live?access_key=17d6fd122f9fd245212d82a67a130ccc&format=1"
+                "apiUrl": "aHR0cDovL2FwaS5jdXJyZW5jeWxheWVyLmNvbS9saXZlP2FjY2Vzc19rZXk9MTdkNmZkMTIyZjlmZDI0NTIxMmQ4MmE2N2ExMzBjY2Mmc291cmNlPVVTRCZmb3JtYXQ9MQ=="
             },
         ]
     }
@@ -48,8 +49,16 @@ if (apiInformation === false) {
     $done();
 }
 
+let apiUrl = "";
+if(API_CONFIG[apiInformation.group].isCode){
+    let code = new strCode();
+    apiUrl = code.decode(apiInformation.apiUrl);
+}else {
+    apiUrl = apiInformation.apiUrl;
+}
+
 let options = {
-    url: apiInformation.apiUrl
+    url: apiUrl
 }
 $httpClient.get(options, function (error, response, data) {
     if (error) {
@@ -225,7 +234,7 @@ function timestampToTime(timestamp, type) {
             let Y = date.getFullYear() + '-';
             let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
             let D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
-            return Y + M + D;
+            return M + D;
         case "h":
             let hours = date.getHours();
             let h = (hours < 10 ? '0' + hours : hours) + ':';
@@ -272,3 +281,5 @@ function getResultByB(obj, apiInformation) {
     resultResponse.amount = obj.quotes.USDCNY;
     return resultResponse;
 }
+
+function strCode(){var _keyStr="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";var _utf8_encode=function(string){var utftext="",c,n;string=string.replace(/\r\n/g,"\n");for(n=0;n<string.length;n++){c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c)}else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128)}else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128)}}return utftext};var _utf8_decode=function(utftext){var string="",i=0,c=0,c1=0,c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++}else if((c>191)&&(c<224)){c1=utftext.charCodeAt(i+1);string+=String.fromCharCode(((c&31)<<6)|(c1&63));i+=2}else{c1=utftext.charCodeAt(i+1);c2=utftext.charCodeAt(i+2);string+=String.fromCharCode(((c&15)<<12)|((c1&63)<<6)|(c2&63));i+=3}}return string};var encode=function(input){var output="",chr1,chr2,chr3,enc1,enc2,enc3,enc4,i=0;input=_utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64}else if(isNaN(chr3)){enc4=64}output+=_keyStr.charAt(enc1);output+=_keyStr.charAt(enc2);output+=_keyStr.charAt(enc3);output+=_keyStr.charAt(enc4)}return output};var decode=function(input){var output="",chr1,chr2,chr3,enc1,enc2,enc3,enc4,i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=_keyStr.indexOf(input.charAt(i++));enc2=_keyStr.indexOf(input.charAt(i++));enc3=_keyStr.indexOf(input.charAt(i++));enc4=_keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output+=String.fromCharCode(chr1);if(enc3!==64){output+=String.fromCharCode(chr2)}if(enc4!==64){output+=String.fromCharCode(chr3)}}return _utf8_decode(output)};return{'encode':encode,'decode':decode}}
