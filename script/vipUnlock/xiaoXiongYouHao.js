388ca204f2356a9d065f7567a9d1f5aea710eaf4
fhqievg <30724814+fhqievg@users.noreply.github.com>
2023-06-22 09:47:05 +0800

    更新 xiaoXiongYouHao.js

--- script/vipUnlock/xiaoXiongYouHao.js
+++ script/vipUnlock/xiaoXiongYouHao.js
@@ -1,4 +1,10 @@
 let obj = JSON.parse($response.body);
-obj.vip_state = 2;
-obj.vip_valid_till_date = "2099\u5e7412\u670831\u65e5";
+let url = $request.url;
+
+if (url.includes("api/vip/index")) {
+    obj.vip_state = 2;
+    obj.vip_valid_till_date = "2099\u5e7412\u670831\u65e5";
+} else if ( url.includes("api/account_sync")) {
+    obj.accountInfo.accountSetting.phone_number = "13012345678";
+}
 $done({ body:JSON.stringify(obj) });
